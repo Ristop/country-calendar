0302a22ac50b88c6a1675fcc4ffc42aeba975b37
Risto Pärnapuu <ristoparnapuu@gmail.com>
2024-11-02 10:29:54 +0200

    use grid borders

--- src/TimeLine.tsx
+++ src/TimeLine.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './TimeLine.scss';
 import { CountryInfo, CountriesByYear } from './App';
 import YearContainer from './YearContainer';
@@ -9,11 +9,47 @@ export interface YearContainerProps {
   firstVisited: CountryInfo[];
 }
 
+function getNumOfColumns(element: Element | null) {
+  if (element) {
+    return window.getComputedStyle(element).getPropertyValue('grid-template-columns').split(' ').length;
+  } else {
+    return 0;
+  }
+}
+
 const TimeLine = ({ years, countries, firstVisited }: YearContainerProps) => {
+  const ref = useRef<HTMLInputElement>(null);
+  const [extraCells, setExtraCells] = useState<number>(0);
+
+  window.addEventListener('resize', () => {
+    const columns = getNumOfColumns(ref.current);
+    const lastRow = years.length % columns;
+
+    if (lastRow === 0) {
+      setExtraCells(0);
+    } else {
+      setExtraCells(columns - lastRow);
+    }
+  });
+
+  window.addEventListener('load', () => {
+    const columns = getNumOfColumns(ref.current);
+    const lastRow = years.length % columns;
+
+    if (lastRow === 0) {
+      setExtraCells(0);
+    } else {
+      setExtraCells(columns - lastRow);
+    }
+  });
+
   return (
-    <div className='timeline'>
+    <div className='timeline' ref={ref}>
       {years.map((year) => (
         <YearContainer key={year} year={year} countries={countries[year]} firstVisited={firstVisited} />
+      ))}
+      {Array.from({ length: extraCells }).map((_, index) => (
+        <div key={index} className='empty-cell'></div>
       ))}
     </div>
   );
